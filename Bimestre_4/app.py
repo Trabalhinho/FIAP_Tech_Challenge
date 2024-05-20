dedbc592f3d7966c4b242f98eaf8dc7724827501
mchaotheus <matheus1augusto1@gmail.com>
2024-05-20 16:56:43 -0300

    too many things

--- Bimestre_4/app.py
+++ Bimestre_4/app.py
@@ -3,6 +3,22 @@ import streamlit as st
 import pandas as pd
 import joblib
 from joblib import load
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+
+
+from sktime.forecasting.fbprophet import Prophet
+from statsforecast import StatsForecast
+from statsforecast.models import Naive
+
+from sktime.datasets import load_airline
+from sktime.forecasting.base import ForecastingHorizon
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.performance_metrics.forecasting import mean_absolute_percentage_error
+from sktime.utils.plotting import plot_series
+
+from statsforecast import StatsForecast
+from statsforecast.models import Naive
 
 #carregando os dados 
 #dados = pd.read_csv('https://raw.githubusercontent.com/alura-tech/alura-tech-pos-data-science-credit-scoring-streamlit/main/df_clean.csv')
@@ -22,7 +38,9 @@ input_idade = st.date_input( 'Selecione a data' )
 
 #Predições 
 if st.button('Prever'):
-    model = joblib.load('modelo/prophet.joblib')
-    final_pred = model.predict(input_idade)
+    model = joblib.load( 'modelo/prophet.joblib' )
+    print("cheguei aqui")
+    final_pred = model.predict(input_idade) 
+    print("mais um")
     print(final_pred)
  
